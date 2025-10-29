6fcecb7b47a5c22695a382081d348b09f67767e4
Malkocheva <lyubima.malkocheva@sap.com>
2025-10-29 11:30:22 +0200

    Input Validation

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
@@ -21,15 +22,43 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
 });
 
+const conversations = new Map<string, string>();
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required.')
+      .max(1000, 'Prompt is too long (max 1000 characters)'),
+   // conversationId: z.string().uuid()
+   conversationId: z
+      .string()
+      .regex(
+         /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
+         'conversationId must be a valid UUID v4'
+      ),
+});
+
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      // res.status(400).json(parseResult.error.format());
+      // return;
+      const formattedError = z.treeifyError(parseResult.error);
+      return res.status(400).json(formattedError);
+   }
+
+   const { prompt, conversationId } = req.body;
 
    const response = await client.responses.create({
       model: 'gpt-4o-mini',
       input: prompt,
       temperature: 0.2,
       max_output_tokens: 100,
+      previous_response_id: conversations.get(conversationId),
    });
+
+   conversations.set(conversationId, response.id);
 
    res.json({ message: response.output_text });
 });
